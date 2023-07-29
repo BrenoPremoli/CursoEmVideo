2fc9a84c69821be5f7dd07813bd402cc1738fed4
Breno <breno.oliveira463@gmail.com>
2023-07-29 12:47:42 -0300

    SQL

--- sql/select.sql
+++ sql/select.sql
@@ -195,9 +195,62 @@ where peso > '100'
 group by altura
 having altura > (select avg(altura) from gafanhotos);
 
+desc cursos;
 
+alter table gafanhotos
+add cursopreferido int;
 
+alter table gafanhotos
+add foreign key (cursopreferido)
+references cursos(idcurso);
 
+update gafanhotos set cursopreferido = '6' 
+where id = '1';
 
+delete from cursos
+where idcurso = '6';
+
+delete from cursos
+where idcurso = '28';
+
+select gafanhotos.nome, gafanhotos.cursopreferido, cursos.nome, cursos.ano 
+from gafanhotos join cursos
+on cursos.idcurso = gafanhotos.cursopreferido;
+
+select gafanhotos.nome, cursos.nome, cursos.ano 
+from gafanhotos inner join cursos
+on cursos.idcurso = gafanhotos.cursopreferido
+order by gafanhotos.nome;
+
+select g.nome, c.nome, c.ano 
+from gafanhotos as g inner join cursos as c
+on c.idcurso = g.cursopreferido
+order by g.nome;
+
+select g.nome, c.nome, c.ano 
+from gafanhotos as g right outer join cursos as c
+on c.idcurso = g.cursopreferido
+order by g.nome;
+
+create table gafanhoto_assiste_curso (
+	id int not null auto_increment,
+    data date,
+    idgafanhoto int,
+    idcurso int,
+    primary key(id),
+    foreign key (idgafanhoto) references gafanhotos(id),
+    foreign key (idcurso) references cursos(idcurso)
+) default charset = utf8;
+
+insert into gafanhoto_assiste_curso values 
+(default, '2014-03-01', '1', '2');
+
+select * from gafanhoto_assiste_curso;
+
+select g.nome, a.idgafanhoto, c.idcurso, c.nome from gafanhotos g
+join gafanhoto_assiste_curso a
+on g.id = a.idgafanhoto
+join cursos c 
+on c.idcurso = a.idcurso;
 
 
